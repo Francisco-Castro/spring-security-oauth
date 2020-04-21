9837aa737723309daae1d93b5c2dceb4f224b39a
Francisco-Castro <zipcodeFrancisco@gmail.com>
2020-04-21 12:15:46 -0400

    Implemented service for User, including logger and handle errors. Added dependencies for Spring Security within POM. Remove public accesor from IUserDao. Added Getters and Setters annotations for Role entity.

--- src/main/java/com/catra/springsecurityoauth/entities/Role.java
+++ src/main/java/com/catra/springsecurityoauth/entities/Role.java
@@ -1,10 +1,15 @@
 package com.catra.springsecurityoauth.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "roles")
 public class Role implements Serializable {
 
